5f58aa6440067744113ba5dc469cbf4517d0d94c
Sandra <sandraa18@ru.is>
2019-05-08 11:12:25 +0000

    Apps, html, images and CSS added into Project

--- CastleApartments/urls.py
+++ CastleApartments/urls.py
@@ -1,4 +1,4 @@
-"""CastleApartments URL Configuration
+"""CastleApt URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("", include("Home.urls")),
+    path("home/", include("Home.urls")),
+    path("login/", include("Login.urls")),
+    path("sign_up/", include("Signup.urls")),
+    path("employees/", include("Employees.urls")),
+    path("real_estate/", include("RealEstate.urls")),
+    path("real_estate_information/", include("RealEstateInformation.urls")),
+    path("add_real_estate/", include("AddRealEstate.urls")),
+    path("add_real_estate_confirmation/", include("AddRealEstateConfirmation.urls")),
+    path("your_real_estate/", include("YourRealEstate.urls")),
+    path("payment_information/", include("PaymentInformation.urls")),
+    path("confirmation/", include("Confirmation.urls")),
+    path("pending/", include("Pending.urls")),
+    path("pending_information/", include("PendingInformation.urls")),
     path('admin/', admin.site.urls),
 ]
