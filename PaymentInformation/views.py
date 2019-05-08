518465bffaae8f463538829dbc84fe92cabe442e
bjorksigur <bjorks18@ru.is>
2019-05-08 22:20:26 +0000

    Payment information

--- PaymentInformation/views.py
+++ PaymentInformation/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from PaymentInformation.forms.payment_information_forms import CreatePaymentform
 # Create your views here.
 
 
 def index(request):
+
     return render(request, 'PaymentInformation/index.html')
+
+
+#def payment_information(request):
+ #   if request.method == 'POST':
+  #      form = CreatePaymentform(data=request.POST)
+  #      if form.is_valid():
+   #         payment = form.save()
+   #         return redirect('confirmation_index')
+   # else:
+   #     form = CreatePaymentform()
+   # return render(request, 'PaymentInformation/index.html', {
+   #     'form': form
+   # })
+
+def payment_information(request):
+    form = CreatePaymentform(data=request.POST)
+    if form.is_valid():
+        payment = form.save()
+        return redirect('confirmation_index')
+    else:
+         form = CreatePaymentform()
+    return render(request, 'PaymentInformation/index.html', {
+         'form': form
+         })
