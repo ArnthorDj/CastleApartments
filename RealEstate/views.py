9c48896d11b705bd92f37e2ac2ce789167e71104
Benjamín_Aage <benjamin18@ru.is>
2019-05-09 12:50:11 +0000

    Database changes

--- RealEstate/views.py
+++ RealEstate/views.py
@@ -4,7 +4,7 @@ from RealEstate.models import RealEstates, RealEstateImages
 
 def index(request):
     context = {'real_estates': RealEstates.objects.all().order_by('street')}
-    images = {'images': RealEstateImages.objects.all()}
-    return render(request, 'RealEstate/index.html', context, images)
+ #   images = {'images': RealEstateImages.objects.all()}
+    return render(request, 'RealEstate/index.html', context)
 
 
