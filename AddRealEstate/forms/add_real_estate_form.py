4d8538218befd85736ecf0ea3672a283831bbbb0
bjorksigur <bjorks18@ru.is>
2019-05-09 22:12:00 +0000

    add_real_estate formið komið, vantar að gera dropdown menu

--- AddRealEstate/forms/add_real_estate_form.py
+++ AddRealEstate/forms/add_real_estate_form.py
@@ -5,16 +5,17 @@ from django import forms
 class AddRealEstateForm(ModelForm):
     class Meta:
         #image = forms.CharField(required=True, widgets=forms.TextInput(attrs={'class':'form-control'}))
-        model: NewRealEstate
+        model = NewRealEstate
         exclude = ['id', 'on_sale', 'seller_id']
         #fields = "__all__"
         widgets = {
-            'street': widgets.TextInput(attrs={'class':'form-control'}),
-            'city': widgets.TextInput(attrs={'class':'form-control'}),
-            'zip_code': widgets.Select(attrs={'class':'form-control'}),
-            'size': widgets.TextInput(attrs={'class':'form-control'}),
-            'bedrooms': widgets.TextInput(attrs={'class':'form-control'}),
-            'type': widgets.Select(attrs={'class':'form-control'}),
-            'price': widgets.TextInput(attrs={'class':'form-control'})
+            'street': widgets.TextInput(attrs={'class':'form-control','style':'max-width: 16em'}),
+            'city': widgets.TextInput(attrs={'class':'form-control','style':'max-width: 16em'}),
+            'zip_code': widgets.Select(attrs={'class':'form-control','style':'max-width: 16em'}),
+            'size': widgets.TextInput(attrs={'class':'form-control','style':'max-width: 16em'}),
+            'bedrooms': widgets.TextInput(attrs={'class':'form-control','style':'max-width: 16em'}),
+            'bathrooms': widgets.TextInput(attrs={'class': 'form-control', 'style': 'max-width: 16em'}),
+            'type': widgets.Select(attrs={'class':'form-control','style':'max-width: 16em'}),
+            'price': widgets.TextInput(attrs={'class':'form-control','style':'max-width: 16em'})
         }
 
