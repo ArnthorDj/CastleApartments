92edd2709052d8b5ae5d97ede49b0f3a06ea1d4f
Benjamín_Aage <benjamin18@ru.is>
2019-05-09 16:36:54 +0000

    NEW Database created

--- AddRealEstate/models.py
+++ AddRealEstate/models.py
@@ -1,20 +1,16 @@
 from django.db import models
-from django import forms
-
+from django.contrib.auth.models import User
 
 
 class NewRealEstate(models.Model):
-    #id = models.IntegerField()
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=99)
     zip_code = models.CharField(max_length=3)
     size = models.CharField(max_length=10)
     bedrooms = models.CharField(max_length=2)
+    bathrooms = models.CharField(max_length=2)
     type = models.CharField(max_length=99)
     price = models.CharField(max_length=999)
     on_sale = models.BooleanField()
-    seller_id = models.IntegerField()
-
-    def __str__(self):
-        return "Street: {}\nCity: {}\nZip Code: {}\nSize: {}\nBedrooms: {}\nType: {}\nPrice: {}".format(self.street, self.city, self.zip_code, self.size, self.bedrooms, self.type, self.price)
+    seller_id = models.OneToOneField(User, on_delete=models.CASCADE)
 
