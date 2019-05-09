e1502a9ff536f4ff56e71bf2851200c152b29051
Your Name <you@example.com>
2019-05-09 20:48:38 +0000

    No update

--- ContactUs/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def contactUs(request):
-    return render(request, 'ContactUs/index.html')
--- Pending/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.pending, name="pending_index"),
-    path("pending_information/", views.pending_information, name="pending_information_index")
-]
