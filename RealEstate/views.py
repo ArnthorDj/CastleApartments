529bc4997d3febb94d8b58bdedcd6c230c7d69eb
Your Name <you@example.com>
2019-05-09 12:41:02 +0000

    Real Estate in a single folder

--- RealEstate/views.py
+++ RealEstate/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from RealEstate.models import RealEstates, RealEstateImages
+from django.http import HttpResponse
 
 
-def index(request):
+def realEstate(request):
     context = {'real_estates': RealEstates.objects.all().order_by('street')}
     images = {'images': RealEstateImages.objects.all()}
     return render(request, 'RealEstate/index.html', context, images)
 
+
+def realEstateInformation(request):
+    return render(request, 'RealEstateInformation/index.html')
+
+
+def addRealEstatem(request):
+    return HttpResponse("Hello from the index function within the AddRealEstate app!")
+
+
+def addRealEstateCofirmation(request):
+    return HttpResponse("Hello from the index function within the AddRealEstateConfirmation app!")
+
+
+def buyConfirmation(request):
+    return HttpResponse("Hello from the index function within the Confirmation app!")
+
+
+def yourRealEstate(request):
+    return HttpResponse("Hello from the index function within the YourRealEstate app!")
 
