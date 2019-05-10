16523ea8d553b8884707f50319c39bf324ed8dab
bjorksigur <bjorks18@ru.is>
2019-05-10 09:44:31 +0000

    færði add_real_estate yfir í real_estate

--- RealEstate/urls.py
+++ RealEstate/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("real_estate_information", views.realEstateInformation, name="real_estate_information"),
     path("real_estate_information/payment_information/", views.payment_information, name="payment_information_index"),
     path("real_estate_information/payment_information/payment_confirmation/", views.payment_confirmation, name="payment_confirmation"),
-    path("add_real_estate/", views.addRealEstatem, name="add_real_estate"),
+    path("add_real_estate/", views.addRealEstate, name="add_real_estate"),
     path("add_real_estate_confirmation/", views.addRealEstateCofirmation, name="add_real_estate_confirmation"),
     path("your_real_estate/", views.yourRealEstate, name="your_real_estate"),
 
--- RealEstate/views.py
+++ RealEstate/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from RealEstate.models import RealEstates, RealEstateImages
 from django.http import HttpResponse
 from PaymentInformation.forms.payment_information_forms import CreatePaymentform
+from RealEstate.forms.add_real_estate_form import AddRealEstateForm
+
 
 
 def index(request):
@@ -14,8 +16,16 @@ def realEstateInformation(request):
     return render(request, 'RealEstateInformation/index.html')
 
 
-def addRealEstatem(request):
-    return HttpResponse("Hello from the index function within the AddRealEstate app!")
+def addRealEstate(request):
+    form = AddRealEstateForm(data=request.POST)
+    if form.is_valid():
+        new_real_estate = form.save()
+        return redirect('confirmation_index')
+    else:
+        form = AddRealEstateForm()
+    return render(request, 'AddRealEstate/index.html', {
+        'form': form
+    })
 
 
 def addRealEstateCofirmation(request):
