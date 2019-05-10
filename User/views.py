681f726c09f5c29c1393ff492537d2bf95dabccc
Your Name <you@example.com>
2019-05-10 15:20:05 +0000

    Profile 90% done

--- User/views.py
+++ User/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from User.models import Profile
-from User.forms.profile_form import ProfileForm
+from User.forms.profile_form import ProfileForm, AuthUser
+from django.contrib.auth.models import User
 
 
 def register(request):
@@ -21,13 +22,17 @@ def register(request):
 
 def profile(request):
     profile = Profile.objects.filter(user=request.user).first()
+    user_form = User.objects.filter(id=request.user.id).first()
     if request.method == "POST":
         form = ProfileForm(instance=profile, data=request.POST)
+        form2 = AuthUser(instance=user_form,   data=request.POST)
         if form.is_valid():
             profile = form.save(commit=False)
             profile.user = request.user
             profile.save()
             return redirect("profile")
-    return render(request, "User/profile.html",{
-        "form": ProfileForm(instance=profile)
+
+    return render(request, "User/profile.html", {
+        "form": ProfileForm(instance=profile),
+        "form2": AuthUser(instance=user_form)
     })
