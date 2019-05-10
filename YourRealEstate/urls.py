555554ce1abdf75d4f67671ff488bd9f3a1a14d8
Sandra <sandraa18@ru.is>
2019-05-10 09:31:49 +0000

    shorten urls

--- YourRealEstate/urls.py
+++ YourRealEstate/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.your_real_estate, name="your_real_estate_index"),
-    path("add_real_estate/", views.add_real_estate, name="add_real_estate_index"),
-    path("add_real_estate/add_real_estate_confirmation/", views.add_real_estate_confirmation, name="add_real_estate_confirmation_index")
+    path("add/", views.add_real_estate, name="add_real_estate_index"),
+    path("add/confirmation/", views.add_real_estate_confirmation, name="add_real_estate_confirmation_index")
 ]
