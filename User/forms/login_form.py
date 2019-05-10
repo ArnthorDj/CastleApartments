659e78734f300059732e59a1e3cf18acd89294f6
bjorksigur <bjorks18@ru.is>
2019-05-10 14:35:46 +0000

    form fyrir login

---
+++ User/forms/login_form.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+    username = forms.EmailField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Username', 'id': 'hello'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': '',
+            'id': 'hi',
+        }
+    ))
+
+
+
