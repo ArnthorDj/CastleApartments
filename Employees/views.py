b15952949667307b491e86e80b4702e89fc4076d
unknown <arnthordj@hotmail.com>
2019-05-12 19:35:27 +0000

    Home Page, base_4.html, base_4.css

--- Employees/views.py
+++ Employees/views.py
@@ -4,8 +4,12 @@ from django.contrib.auth.models import User
 
 
 def index(request):
-    context = {'employees': Profile.objects.all().filter(user__is_staff=True)}
-    for employee in context['employees']:
-        print(employee.auth_user_password_set)
-    print("THIS IS THE QUERYT")
+    #context = {'employees': Profile.objects.select_related('user').filter(user__is_staff=True)}
+
+    #context2 = Profile.objects.select_related('user').all()
+    #for project in context2:
+    context = {'employees': User.objects.select_related('profile').filter(is_staff=True)}
+    #for u in context['employees']:
+        #print(u.first_name, u.profile.phone)
+
     return render(request, 'Employees/index.html', context)
