204aa613b90cd5a128aafcd7a8df4cb32acb5357
Benjamín_Aage <benjamin18@ru.is>
2019-05-13 12:27:17 +0000

    Database fixed and Employee-app

--- Employees/views.py
+++ Employees/views.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 
 def index(request):
-    context = {'employees': User.objects.select_related('profile').filter(is_staff=True)}
+    context = {'employees': User.objects.select_related('profile').filter(is_staff=True).order_by('first_name')}
     return render(request, 'Employees/index.html', context)
