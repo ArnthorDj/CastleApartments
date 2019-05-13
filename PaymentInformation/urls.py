657ed62161bdd023368dbaec086ebcd1a031d769
Your Name <you@example.com>
2019-05-13 09:30:56 +0000

    Deleting useless apps

--- PaymentInformation/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-#urlpatterns = [
-  #  path("", views.payment_information, name="payment_information_index"),
- #   #path('payment_information', views.payment_information, name='payment_information')
-#]
