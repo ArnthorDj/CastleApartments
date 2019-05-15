52633532d166ff261e0f36df493fcc35a6e9efff
mariak18 <mariak18@ru.is>
2019-05-15 10:47:50 +0000

    :)

--- static/js/search.js
+++ static/js/search.js
@@ -13,10 +13,10 @@ $(document).ready(function() {
                 return `<div class="well real estate">
                             <a href="/real_estate/info/$(d.id)">
                                 <img src="${d.main_image}" height="200" width="300"> 
-                                <h4 style="color: black">${d.street}, ${d.zip_code_id}, ${d.city}</h4>
-                                <p style="color: black">Bedrooms: ${d.bedrooms}, Bathrooms: ${d.bathromms}, Size: ${d.size} square meters</p>
-                                <p style="color: black">Type: ${d.type}</p>
-                                <p style="color: black">${d.price} kr.</p>
+                                <h4 class="black-font">${d.street}, ${d.zip_code_id}, ${d.city}</h4>
+                                <p class="black-font">Bedrooms: ${d.bedrooms}, Bathrooms: ${d.bathromms}, Size: ${d.size} square meters</p>
+                                <p class="black-font">Type: ${d.type}</p>
+                                <p class="black-font">${d.price} kr.</p>
                             </a>
             
                          </div>`
