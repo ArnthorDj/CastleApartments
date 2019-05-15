e4f9e2d712e22e8fef4d3135f68719dca617f80f
Your Name <you@example.com>
2019-05-15 14:48:19 +0000

    Profile and payment confirmation

--- RealEstate/forms/payment_information_form.py
+++ RealEstate/forms/payment_information_form.py
@@ -1,31 +1,30 @@
 from django.forms import ModelForm, widgets
 from User.models import CreditCard
+import datetime
 
-
-MONTHS = {
-    '1':'January',
-    '2':'February',
-    '3':'March',
-    '4':'April',
-    '5':'May',
-    '6':'June',
-    '7':'July',
-    '8':'August',
-    '9':'September',
-    '10':'October',
-    '11':'November',
-    '12':'December'
-    }
 
 
 class CreatePaymentForm(ModelForm):
     class Meta:
         model = CreditCard
-        fields = '__all__'
+        # fields = '__all__'
         exclude = ['user']
+        year = int(datetime.datetime.now().year)
         widgets = {
             'card_number': widgets.TextInput(attrs={'class': 'form-control', 'placeholder':'Card number'}),
-            'month': widgets.SelectDateWidget(months=MONTHS, years=None, attrs={'class': 'form-control', 'placeholder': 'Month'}),
-            'year': widgets.Select(attrs={'class': 'form-control', 'placeholder': 'Year'}),
+            'month': widgets.Select(choices=[
+                ('1', 'January'),
+                ('2', 'February'),
+                ('3', 'March'),
+                ('4', 'April'),
+                ('5', 'May'),
+                ('6', 'June'),
+                ('7', 'July'),
+                ('8', 'August'),
+                ('9', 'September'),
+                ('10', 'October'),
+                ('11', 'November'),
+                ('12', 'December')], attrs={'class': 'form-control', 'placeholder': 'Month'}),
+            'year': widgets.Select(choices=[(year, year) for year in range(year, year+5)], attrs={'class': 'form-control', 'placeholder': 'Year'}),
             'cvc': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'CVC'})
         }
--- RealEstate/urls.py
+++ RealEstate/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="real_estate"),
     path("info/<int:id>", views.get_real_estate_by_id, name="real_estate_information"),
-    path("pay", views.payment_information, name="payment_information_index"),
-    path("info/pay/confirmation/", views.payment_confirmation, name="payment_confirmation"),
+    path("pay/<int:id>", views.payment_information, name="payment_information_index"),
+    path("pay/<int:id>/confirmation/", views.payment_confirmation, name="payment_confirmation"),
     # path("add/", views.addRealEstate, name="add_real_estate"),
     # path("add/confirmation/", views.addRealEstateConfirmation, name="add_real_estate_confirmation"),
     # path("your_real_estate/", views.yourRealEstate, name="your_real_estate"),
