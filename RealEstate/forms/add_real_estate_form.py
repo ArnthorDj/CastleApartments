96870cda5cc49e659a5fb67e6b93e759f12e64cc
Your Name <you@example.com>
2019-05-16 09:39:21 +0000

    Merge

--- RealEstate/forms/add_real_estate_form.py
+++ RealEstate/forms/add_real_estate_form.py
@@ -1,20 +1,41 @@
-# from django.forms import ModelForm, widgets
-# from RealEstate.models import RealEstates
-# from django import forms
+from django.forms import ModelForm, widgets
+from RealEstate.models import RealEstates, RealEstateImages
 
-# class AddRealEstateForm(ModelForm):
-# class Meta:
-# image = forms.CharField(required=True, widgets=forms.TextInput(attrs={'class':'form-control'}))
-# model = RealEstates
-# exclude = ['id', 'on_sale', 'seller_id']
-# fields = "__all__"
-# widgets = {
-# 'street': widgets.TextInput(attrs={'class':'form-control','style':'max-width: 16em'}),
-# 'city': widgets.TextInput(attrs={'class':'form-control','style':'max-width: 16em'}),
-# 'zip_code': widgets.Select(attrs={'class':'form-control','style':'max-width: 16em'}),
-# 'size': widgets.TextInput(attrs={'class':'form-control','style':'max-width: 16em'}),
-# 'bedrooms': widgets.TextInput(attrs={'class':'form-control','style':'max-width: 16em'}),
-# 'bathrooms': widgets.TextInput(attrs={'class': 'form-control', 'style': 'max-width: 16em'}),
-# 'type': widgets.Select(attrs={'class':'form-control','style':'max-width: 16em'}),
-# 'price': widgets.TextInput(attrs={'class':'form-control','style':'max-width: 16em'})
-# }
+
+class AddRealEstateForm(ModelForm):
+    class Meta:
+        # image = forms.CharField(required=True, widgets=forms.TextInput(attrs={'class':'form-control'}))
+        model = RealEstates
+        exclude = ['id', 'on_sale', 'seller_id', 'seller']
+        # fields = "__all__"
+        widgets = {
+            'street': widgets.TextInput(attrs={'class': 'form-control', 'style': 'max-width: 20em'}),
+            'city': widgets.TextInput(attrs={'class': 'form-control', 'style': 'max-width: 20em'}),
+            'zip_code': widgets.Select(attrs={'class': 'form-control', 'style': 'max-width: 20em'}),
+            'size': widgets.TextInput(attrs={'class': 'form-control', 'style': 'max-width: 20em'}),
+            'bedrooms': widgets.TextInput(attrs={'class': 'form-control', 'style': 'max-width: 20em'}),
+            'bathrooms': widgets.TextInput(attrs={'class': 'form-control', 'style': 'max-width: 20em'}),
+            'type': widgets.Select(choices=[
+                ("Beach House", "Beach House"),
+                ("Single Family Home", "Single Family Home"),
+                ("Summer House", "Summer House"),
+                ("Condo House", "Condo House"),
+                ("Apartment Building", "Apartment Building"),
+                ("Townhouse", "Townhouse"),
+                ("Villas", "Villas")
+            ] ,attrs={'class': 'form-control','choices':'COLOR_CHOICES', 'style': 'max-width: 20em'}),
+            'price': widgets.TextInput(attrs={'class': 'form-control', 'style': 'max-width: 20em'}),
+            'more_info': widgets.Textarea(attrs={'class': 'form-control', 'style': 'width: 20em; height: 10em;'}),
+            'main_image': widgets.TextInput(attrs={'class': 'form-control', 'style': 'max-width: 20em'})
+        }
+
+
+class AddRealEstateImage(ModelForm):
+    class Meta:
+        # image = forms.CharField(required=True, widgets=forms.TextInput(attrs={'class':'form-control'}))
+        model = RealEstateImages
+        # fields = "__all__"
+        exclude = ['id', 'real_estate']
+        widgets = {
+            'images': widgets.TextInput(attrs={'class': 'form-control', 'style': 'max-width: 20em'})
+        }
