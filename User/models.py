c65a708b5a994b5021ea6d10af9dde9cacd50667
Benjamín_Aage <benjamin18@ru.is>
2019-05-17 14:35:48 +0000

    User app bulletproof

--- User/models.py
+++ User/models.py
@@ -4,6 +4,7 @@ from RealEstate.models import RealEstates, ZipCodes
 
 
 class Profile(models.Model):
+    """ Variables (columns) for the User_Profile table in database. """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.CharField(max_length=999, blank=True)
     street = models.CharField(max_length=99)
@@ -13,12 +14,14 @@ class Profile(models.Model):
 
 
 class Purchases(models.Model):
+    """ Variables (columns) for the User_Purchases table in database. """
     buyer = models.CharField(max_length=99)
     seller = models.CharField(max_length=99)
     real_estate = models.CharField(max_length=99)
 
 
 class CreditCard(models.Model):
+    """ Variables (columns) for the User_CreditCard table in database. """
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     card_number = models.CharField(max_length=16)
     month = models.CharField(max_length=2)
@@ -26,6 +29,7 @@ class CreditCard(models.Model):
 
 
 class UserHistory(models.Model):
+    """ Variables (columns) for the UserHistory table in database. """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     real_estate = models.ForeignKey(RealEstates, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True, blank=True)
