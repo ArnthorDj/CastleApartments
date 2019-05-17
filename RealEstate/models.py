bcf7c04e934df96f1fc21f128658588eb57add96
Benjamín_Aage <benjamin18@ru.is>
2019-05-17 16:10:18 +0000

    RealEstate models.py double-bulletproof

--- RealEstate/models.py
+++ RealEstate/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class ZipCodes(models.Model):
-    """ Here are information about zip codes, which are stored in the Real_Estate_zipcodes table."""
+    """ Here are information about zip codes, which are stored in the Real_Estate_zip_codes table."""
 
     zip_code = models.CharField(max_length=3, primary_key=True)
     city = models.CharField(max_length=50)
@@ -11,7 +11,7 @@ class ZipCodes(models.Model):
 
 
 class RealEstates(models.Model):
-    """ Here are information about the real estate, which are stored in Real_Estate_realestates table.
+    """ Here are information about the real estate, which are stored in Real_Estate_real_estates table.
         Alse retrieving information about zip-code (zip code, city, country) and the Employee (User)
         that created the real estate with foreign key reference"""
 
@@ -25,12 +25,14 @@ class RealEstates(models.Model):
     on_sale = models.BooleanField()
     more_info = models.CharField(max_length=999)
     employee = models.ForeignKey(User, on_delete=models.CASCADE)
-    main_image = models.CharField(max_length=999, default="https://image.shutterstock.com/image-vector/simple-house-icon-white-flat-260nw-1086201605.jpg")
+    main_image = models.CharField(
+        max_length=999,
+        default="https://image.shutterstock.com/image-vector/simple-house-icon-white-flat-260nw-1086201605.jpg")
 
 
 class RealEstateImages(models.Model):
-    """ Hera are iformation about real estate images, which are stored in the Real_Estate_realestateimages.
-        Also retrieving information about the real estate (Real_Estate_realestates) that 'owns' the images
+    """ Hera are information about real estate images, which are stored in the Real_Estate_real_estate_images.
+        Also retrieving information about the real estate (Real_Estate_real_estates) that 'owns' the images
         with foreign key reference """
 
     real_estate = models.ForeignKey(RealEstates, on_delete=models.CASCADE)
