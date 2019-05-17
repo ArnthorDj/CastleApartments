56adf21ec0800fd71b1dbb91fe0bfcba9970df53
Sandra <sandraa18@ru.is>
2019-05-17 11:05:45 +0000

    comment out pending and your real estate

--- Pending/urls.py
+++ Pending/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.pending, name="pending_index"),
-    path("info/", views.pending_information, name="pending_information_index"),
-    path("zip/", views.pending_zip, name="pending_zip"),
-    path("street/", views.pending_street, name="pending_street"),
-    path("size/", views.pending_size, name="pending_size"),
-    path("price/", views.pending_price, name="pending_price"),
-    path("city/", views.pending_city, name="pending_city"),
-]
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#     path("", views.pending, name="pending_index"),
+#     path("info/", views.pending_information, name="pending_information_index"),
+#     path("zip/", views.pending_zip, name="pending_zip"),
+#     path("street/", views.pending_street, name="pending_street"),
+#     path("size/", views.pending_size, name="pending_size"),
+#     path("price/", views.pending_price, name="pending_price"),
+#     path("city/", views.pending_city, name="pending_city"),
+# ]
