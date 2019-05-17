c14cc1a30459a44885436d7fd374023053a80760
Benjamín_Aage <benjamin18@ru.is>
2019-05-17 16:06:25 +0000

    RealEstate views.py double-bulletproof

--- RealEstate/models.py
+++ RealEstate/models.py
@@ -3,18 +3,12 @@ from django.contrib.auth.models import User
 
 
 class ZipCodes(models.Model):
-    """ Here are information about zip codes, which are stored in the Real_Estate_zipcodes table."""
-
     zip_code = models.CharField(max_length=3, primary_key=True)
     city = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
 
 
 class RealEstates(models.Model):
-    """ Here are information about the real estate, which are stored in Real_Estate_realestates table.
-        Alse retrieving information about zip-code (zip code, city, country) and the Employee (User)
-        that created the real estate with foreign key reference"""
-
     street = models.CharField(max_length=255)
     zip_code = models.ForeignKey(ZipCodes, on_delete=models.CASCADE)
     size = models.CharField(max_length=10)
@@ -25,14 +19,12 @@ class RealEstates(models.Model):
     on_sale = models.BooleanField()
     more_info = models.CharField(max_length=999)
     employee = models.ForeignKey(User, on_delete=models.CASCADE)
-    main_image = models.CharField(max_length=999, default="https://image.shutterstock.com/image-vector/simple-house-icon-white-flat-260nw-1086201605.jpg")
+    main_image = models.CharField(
+        max_length=999,
+        default="https://image.shutterstock.com/image-vector/simple-house-icon-white-flat-260nw-1086201605.jpg")
 
 
 class RealEstateImages(models.Model):
-    """ Hera are iformation about real estate images, which are stored in the Real_Estate_realestateimages.
-        Also retrieving information about the real estate (Real_Estate_realestates) that 'owns' the images
-        with foreign key reference """
-
     real_estate = models.ForeignKey(RealEstates, on_delete=models.CASCADE)
     image = models.CharField(max_length=9999)
     image2 = models.CharField(max_length=9999)
