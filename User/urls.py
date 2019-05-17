54429d0d512ff187b4938d0f5f0468f49e41d6a2
Your Name <you@example.com>
2019-05-17 15:49:40 +0000

    User app double bulletproof

--- User/urls.py
+++ User/urls.py
@@ -6,16 +6,22 @@ from User.forms.login_form import LoginForm
 
 # url: /user/
 urlpatterns = [
+
     # /user/register/
     path("register/", views.register, name="register"),
+    +
     # /user/history/
     path("history/", views.user_history, name="user_history"),
+
     # /user/delete_history/
     path("delete_history/", views.delete_history, name="delete_history"),
+
     # /user/logout/
     path("logout/", LogoutView.as_view(next_page='login'), name="logout"),
+
     # /user/profile/
     path("profile", views.profile_update, name="profile"),
+
     # /user/login/
     path(
         'login/', LoginView.as_view( template_name="User/login.html", authentication_form=LoginForm),
--- User/views.py
+++ User/views.py
@@ -8,13 +8,14 @@ from django.contrib.auth.decorators import login_required
 
 
 def register(request):
-    """ Function that adds a new member when user chooses to create a new account. """
+    """ Function that adds a new member """
 
     if request.method == "POST":
         auth_user_form = AuthUserForm(data=request.POST)
         user_profile_form = UserProfile(data=request.POST)
 
         if auth_user_form.is_valid() and user_profile_form.is_valid():
+
             # Gets the ssn-input from user.
             ssn = str(user_profile_form.cleaned_data.get('ssn'))
 
@@ -37,13 +38,13 @@ def register(request):
             username = auth_user_form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
 
-            # Redirects user to login page, for him/her to sign in.
+            # Redirects user to login page, for the user to sign in.
             return redirect("login")
     else:
         auth_user_form = AuthUserForm()
         user_profile_form = UserProfile()
 
-    # Renders the user to the same page with the form he/she has to change to be valid.
+    # Renders the user to the same page with the form the user has to change to be valid.
     return render(request, 'User/register.html', {
         "auth_user_form": auth_user_form,
         "user_profile_form": user_profile_form,
@@ -52,7 +53,8 @@ def register(request):
 
 @login_required
 def profile_update(request):
-    """ Function so user has the availability to change/update already signed information about himself/herself. """
+    """ Function so user has the availability to change/update
+        already signed information about himself/herself. """
 
     profile = Profile.objects.filter(user=request.user).first()
     user = User.objects.filter(id=request.user.id).first()
@@ -61,6 +63,7 @@ def profile_update(request):
         profile_form = ProfileForm(instance=profile, data=request.POST)
         auth_user_form = AuthUser(instance=user,   data=request.POST)
         if profile_form.is_valid() and auth_user_form.is_valid():
+
             # Gets the ssn-input from user.
             ssn = str(profile_form.cleaned_data.get('ssn'))
 
@@ -71,6 +74,7 @@ def profile_update(request):
             if len(ssn) != 10 or not ssn.isdigit():
                 messages.warning(request, f'SSN is not valid (10 numbers)!')
                 return redirect('profile')
+
             profile = profile_form.save(commit=False)
             profile.user = request.user
             profile.save()
@@ -88,7 +92,11 @@ def profile_update(request):
 @login_required
 def user_history(request):
     """ Gets user history. """
+
+    # Gets the user history from the database
     user_history_real_estate = UserHistory.objects.prefetch_related('real_estate').filter(user_id=request.user)
+
+    # Goes to the user history web page with the user history information
     return render(request, 'UserHistory/index.html', {
         'real_estates': user_history_real_estate
     })
@@ -97,5 +105,9 @@ def user_history(request):
 @login_required
 def delete_history(request):
     """ Deletes user history. """
+
+    # Delets all information in the database with user_id equal to the current users id
     UserHistory.objects.filter(user_id=request.user).delete()
+
+    # Goes to the user history web page
     return redirect("user_history")
