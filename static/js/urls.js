a84aff61ada8228e53df963889a4dd39d165a29a
bjorksigur <bjorks18@ru.is>
2019-05-17 16:10:59 +0000

    fínpúss

--- static/js/urls.js
+++ static/js/urls.js
@@ -1,4 +1,4 @@
-// JavaScript file to get different urls for different order by outcomes
+// JavaScript file to get different urls for different order by outcomes.
 
 function real_estate_urls() {
   window.location = document.getElementById("real_estate_order").value
